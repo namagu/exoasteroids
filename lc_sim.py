6662a20c14201ba40c01178160044d2e5aaf6a06
Natalia Guerrero <nmg@mit.edu>
2021-10-18 23:43:10 -0400

    can make and plot light curves with diagnostics

--- lc_sim.py
+++ lc_sim.py
@@ -9,7 +9,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import sys
-import astropy.units as u
+from astropy import units as u
+from astropy import constants as const
 
 #---------------------------------------------------------------
 #             Set up planet parameters
@@ -17,49 +18,96 @@ import astropy.units as u
 print("Setting up star, planet model")
 
 # TODO: be fancy and pull from tango input.py
-rstar = 1.
+rstar = const.R_sun
+mstar = const.M_sun # solar mass
 
 # Keep P, a/Rstar, e, omega constant, vary t0, Rp/Rstar, inc.; Pjup = 100d
 
+def get_a(p):
+  """
+  Calculate semi-major axis
+  
+  p: period [days], Quantity
+  
+  Returns a in AU, Quantity
+  
+  """
+  G = const.G # AU^3 Msun^-1 days^-2
+  a = ((G * (mstar + 0.1*mstar) * (p.to(u.s)**2)) / (4 * (np.pi)**2))**(1/3)
+  return a.to(u.au)
+
+def get_b(inc, a):
+  """
+  Calculate impact parameter
+  
+  inc: inclination [deg], Quantity
+  a: semi-major axis [AU], Quantity 
+  
+  Returns b, dimensionless Quantity
+  """
+  b = (np.cos(np.deg2rad(inc))*a) / rstar
+  return b.decompose()
+
+def get_tdur(p, rp, a, inc):
+  '''
+  Calculate transit duration (in days)
+  
+  p: period [days]
+  rp: planet radius [R_sun]
+  a: semi-major axis [AU]
+  inc: inclination [deg]
+  
+  '''
+  dur = ((p/np.pi)*(np.sqrt((rstar + rp)**2 - (get_b(inc,a)*rstar)**2) / a))
+  return dur.decompose().to(u.hr) 
+
 # FYI 5 AU = 1120 Rsun
 
-params = batman.TransitParams()
-params.t0 = 0.                      #time of inferior conjunction
-params.per = 100                     #orbital period (Kepler-10 c: 45.2942679 d)
-params.rp = 0.1 * rstar             #planet radius (in units of stellar radii)
-params.a = 10                        #semi-major axis (in units of stellar radii)
-params.inc = 90 - 1.305              #orbital inclination (in degrees)
-params.ecc = 0.                      #eccentricity
-params.w = 90.                       #longitude of periastron (in degrees)
-params.u = [0.1, 0.3]            #limb darkening coefficients [u1, u2]
-params.limb_dark = "quadratic"       #limb darkening model
+def get_quantity_value(q):
+  return(q.value)
 
-t = np.arange(0, 1000, 0.0001)
+
+params = batman.TransitParams()
+params.t0 = get_quantity_value(0. * u.day )             #time of inferior conjunction
+params.per = get_quantity_value(100 * u.day)            #orbital period (Kepler-10 c: 45.2942679 d)
+params.rp = get_quantity_value((0.1 * rstar).to(u.R_sun))             #planet radius (in units of stellar radii)
+params.a = get_quantity_value(get_a(params.per * u.day).to(u.R_sun))        #semi-major axis (in units of stellar radii)
+params.inc = 90 # get_quantity_value((90 - 1.305) * u.deg)   #orbital inclination (in degrees)
+params.ecc = 0.                     #eccentricity
+params.w = get_quantity_value(90. * u.deg)              #longitude of periastron (in degrees)
+params.u = [0.1, 0.3]               #limb darkening coefficients [u1, u2]
+params.limb_dark = "quadratic"      #limb darkening model
+
+t = np.arange(0,1000, 0.0001) # * u.day
 # make a data point every 2.4 hours
 
 m = batman.TransitModel(params, t)      # initializes model
 plan_flux = m.light_curve(params) - 1.  # calculates light curve, start from 0 baseline
 
+p_tdur = get_tdur(params.per * u.day, params.rp * u.R_sun, params.a * u.R_sun, params.inc * u.deg)
+
+print(f"planet a: {get_a(params.per * u.day):0.5}, planet b: {get_b(params.inc * u.deg, params.a * u.R_sun):0.5}")
+print(f"Planet T_dur should be {p_tdur:0.5}")
 
 #---------------------------------------------------------------
 #             Set up asteroid ensemble parameters
 #               Greeks: leading
 #               Trojans: trailing
 #---------------------------------------------------------------
-print("Setting up asteroid model")
-
 
 n_trojans = 100  #100     # this is a starting point
 n_greeks = 100  #100
 
-dpd = float(params.per) / 360.  # days per degree
+print(f"Setting up asteroid model for {n_trojans} trailing, {n_greeks} leading")
 
-rarstar = 0.0024973   # r_asteroid [Rsun] / r_star [Rsun] (r_moon = 0.27 * r_earth)
+dpd = params.per / 360.  # days per degree
+
+rarstar = (0.27 * const.R_earth)/const.R_sun  # r_asteroid [Rsun] / r_star [Rsun] (r_moon = 0.27 * r_earth)
 
 # roughly 25 deg spread in phase
-t0_scatter = np.random.uniform(-12.5*dpd,12.5*dpd,1000)  # days
-t_t0_choices = params.t0 - 60*dpd + t0_scatter
-g_t0_choices = params.t0 + 60*dpd + t0_scatter
+t0_scatter = np.random.uniform((-12.5)*dpd, (12.5 )*dpd,1000) # days
+t_t0_choices = params.t0 - (60)*dpd + t0_scatter
+g_t0_choices = params.t0 + (60)*dpd + t0_scatter
 
 #random scatter in inclination
 inc_choices = np.random.normal(params.inc,10,1000) # FYI 13.7 deg is mean for Jupiter trojans 
@@ -72,13 +120,16 @@ all_g_b = []
 
 debug = False   # if True, prints inc, b for each asteroid and adds them to lc plot
 
+t_transits = 0
+g_transits = 0
+
 print("Creating trojans")
 for trojan in range(n_trojans):
   t_params = batman.TransitParams()
   # t0 = t0_planet - trojan phase +/- random scatter
   t_params.t0 = np.random.choice(t_t0_choices,1,replace=False)[0]
-  t_params.per = 100.
-  t_params.rp = rarstar * rstar 
+  t_params.per = 100. 
+  t_params.rp = get_quantity_value(rarstar * rstar.to(u.R_sun))
   t_params.a = params.a   
   t_params.inc = np.random.choice(inc_choices,1,replace=False)[0]
   #t_params.inc = params.inc - np.random.choice(inc_scatter,1,replace=False)[0]
@@ -87,16 +138,24 @@ for trojan in range(n_trojans):
   t_params.u = [0.1,0.3]            #limb darkening coefficients [u1, u2]
   t_params.limb_dark = "quadratic"       #limb darkening model
   t_m = batman.TransitModel(t_params, t)
-  t_flux = t_m.light_curve(t_params) - 1.  # set baseline flux = 0
-  trojans.append(t_flux)
-  #t_b = (np.sin(t_params.inc)*t_params.a) / rstar     # impact parameter
-  t_b = (np.cos(np.deg2rad(t_params.inc))*t_params.a) / rstar
-  all_t_b.append(t_b)
+  
+  t_b = get_b(t_params.inc * u.deg, t_params.a * u.R_sun) 
+  all_t_b.append(t_b)     # save impact parameter
+  
+  if np.abs(t_b) < 1:
+    t_flux = t_m.light_curve(t_params) - 1.  # set baseline flux = 0
+    trojans.append(t_flux)
+    t_transits +=1
+  else:
+    trojans.append(np.zeros(len(t)))
+  
   if debug:
     print(f"t0 = {t_params.t0}d = {params.t0} - {60*dpd} trojan shift + {t0_scatter} scatter")
     print(f"trojan {trojan} inc: {t_params.inc}, a: {t_params.a}, b = {t_b}")
-    
-np.savez('trojan_array',trojans=trojans,all_t_b=all_t_b,t_t0_choices = t_t0_choices)
+
+print(f"{t_transits}/{n_trojans} trailing asteroids have |b|<1")
+print("Saving trojans")
+np.savez('trojan_array',trojans = trojans, all_t_b = all_t_b, t_t0_choices = t_t0_choices, n_t_visible = [t_transits, n_trojans])
 
 print("Creating greeks")
 for greek in range(n_greeks):
@@ -104,7 +163,7 @@ for greek in range(n_greeks):
   # t0 = t0_planet + greek phase +/- random scatter
   g_params.t0 = np.random.choice(g_t0_choices,1,replace=False)[0]
   g_params.per = 100.
-  g_params.rp = rarstar * rstar # * 1e-5 is 10 km
+  g_params.rp = get_quantity_value(rarstar * rstar.to(u.R_sun))
   g_params.a = params.a  
   g_params.inc = np.random.choice(inc_choices,1,replace=False)[0]
   g_params.ecc = params.ecc
@@ -112,17 +171,25 @@ for greek in range(n_greeks):
   g_params.u = [0.1,0.3]            #limb darkening coefficients [u1, u2]
   g_params.limb_dark = "quadratic"       #limb darkening model
   g_m = batman.TransitModel(g_params, t)
-  g_flux = g_m.light_curve(g_params) - 1.  # set baseline flux = 0
-  greeks.append(g_flux)
-  #g_b = (np.sin(g_params.inc)*g_params.a) / rstar     # impact parameter
-  g_b = (np.cos(np.deg2rad(g_params.inc))*g_params.a) / rstar
+  
+  g_b = get_b(g_params.inc * u.deg, g_params.a * u.R_sun) # impact parameter
   all_g_b.append(g_b)
+  
+  if np.abs(g_b) < 1:
+    g_flux = g_m.light_curve(g_params) - 1.  # set baseline flux = 0
+    greeks.append(g_flux)
+    g_transits +=1
+  else:
+    greeks.append(np.zeros(len(t)))  
 
   if debug:
     print(f"t0 = {t_params.t0}d = {params.t0} - {60*dpd} trojan shift + {t0_scatter} scatter")
     print(f"greek {greek} inc: {g_params.inc}, a: {g_params.a}, b = {g_b}")
-          
-np.savez('greek_array',greeks=greeks,all_g_b=all_g_b,g_t0_choices=g_t0_choices)
+
+print(f"{g_transits}/{n_greeks} leading asteroids have |b|<1")
+
+print("Saving greeks")
+np.savez('greek_array',greeks = greeks, all_g_b = all_g_b, g_t0_choices = g_t0_choices, n_g_visible = [g_transits, n_greeks])
 
 print("Putting it all together")
 asteroids = np.append(greeks,trojans,axis=0)
--- plot_lc_sim.py
+++ plot_lc_sim.py
@@ -9,7 +9,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import sys
-import astropy.units as u
+from astropy import units as u
+from astropy import constants as const
+# import lc_sim
 
 debug = False 
 
@@ -17,7 +19,8 @@ debug = False
 dpd = 100 / 360.  # days per degree
 
 # Other parameters
-rarstar = 0.0024973   # r_asteroid [Rsun] / r_star [Rsun] (r_moon = 0.27 * r_earth)
+rarstar = (0.27 * const.R_earth)/const.R_sun   # r_asteroid [Rsun] / r_star [Rsun] (r_moon = 0.27 * r_earth)
+pl_inc = 90. # 90 - 1.305
 
 # 3 diagnostic histograms on top, 
 # 1 lc window on botom
@@ -31,37 +34,47 @@ ax_t0 = fig.add_subplot(gs[0,0])
 ax_inc = fig.add_subplot(gs[0,1])
 ax_b = fig.add_subplot(gs[0,2])
 
-#a,b,c = ax_t0.hist(np.abs(t_t0_choices),30,density=True)
-#ax_t0.set_title("|t0 phase shift| distribution [days]")
-#d,e,f = ax_inc.hist(inc_choices,30,density=True)
-#ax_inc.set_title("inclination distribution [deg]")
-#ax_inc.vlines(params.inc,0,0.05,linestyle='dotted')
-
 # load in data
 print("loading in data")
 lc_data = np.load('lc_flux.npz')
-g_b_data = np.load('greek_array.npz')
-t_b_data = np.load('trojan_array.npz')
+g_data = np.load('greek_array.npz')
+t_data = np.load('trojan_array.npz')
+inc_data = np.load('inc_choices.npz')
 
 # set up variables
 t = np.arange(0, 1000, 0.0001)
-all_g_b = g_b_data['all_g_b']
-all_t_b = t_b_data['all_t_b']
-lc_everything = np.sum(lc_data['everything'],axis=0)
+all_g_b = g_data['all_g_b']
+all_t_b = t_data['all_t_b']
+t_t0_choices = t_data['t_t0_choices']
+inc_choices = inc_data['inc_choices']
+lc_everything = lc_data['everything']
+
+print("plotting")
+
+# t0 histogram, just for trojans, but representative of greeks also
+a,b,c = ax_t0.hist(np.abs(t_t0_choices),30,density=True)
+ax_t0.set_title("|t0 phase shift| distribution [days]")
+
+# inclination histogram
+d,e,f = ax_inc.hist(inc_choices,30,density=True)
+ax_inc.set_title("inclination distribution [deg]")
+ax_inc.vlines(pl_inc,0,0.05,linestyle='dotted')
 
 #ax.plot(t,t_flux,label=f"t {trojan} b = {t_b:0.3f}")
 #ax.plot(t,g_flux,label=f"g {greek} b = {g_b:0.2f}")
 
-print("plotting")
+# impact parameters
 i,j,k = ax_b.hist(np.append(all_g_b, all_t_b, axis=0),30,density=True)
 ax_b.set_title("impact parameter distribution")
-ax_b.vlines(1,0,1,linestyle='dotted')
+ax_b.vlines(1,0, len(all_g_b)*2/30/100,linestyle='dotted')
+ax_b.vlines(-1,0,len(all_g_b)*2/30/100,linestyle='dotted')
 
 ax.plot(t,lc_everything,label="summed lc")
 
 if debug:
-  ax.plot(t,plan_flux,label="planet")
-ax.ticklabel_format(style='sci',scilimits=[-1,1]) 
+  ax.plot(t,lc_data['plan_flux'],label="planet")
+
+  ax.ticklabel_format(style='sci',scilimits=[-1,1]) 
 # transit limits
 ax.vlines([100 - 60*dpd-12.5*dpd, 100 - 60*dpd+12.5*dpd],-5e-5,1e-5,linestyle='dotted',alpha=0.5)
 ax.vlines([100 + 60*dpd-12.5*dpd, 100 + 60*dpd+12.5*dpd],-5e-5,1e-5,linestyle='dotted',alpha=0.5)
@@ -70,11 +83,15 @@ ax.vlines([100 - 60*dpd, 100 + 60*dpd],-5e-5,1e-5,linestyle='dotted',alpha=0.9)
 ax.text(110, -1e-5,"L4 transit")
 ax.text(80, -1e-5, "L5 transit")
 
-ax.set_ylim(-200e-6,10e-6)
-ax.set_xlim(50,150) #150)
+# how many of each transited
+ax.text(80, -6e-5, f"{t_data['n_t_visible'][0]}/{t_data['n_t_visible'][1]} trailing asteroids have |b|<1")
+ax.text(105, -6e-5, f"{g_data['n_g_visible'][0]}/{g_data['n_g_visible'][1]} leading asteroids have |b|<1")
+
+ax.set_ylim(lc_everything.min() * 0.01, 10e-6)
+ax.set_xlim(100 - 25, 100 + 25) #150)
 ax.set_xlabel("Time [d]")
 ax.set_ylabel("Relative flux")
-ax.text(100,0.5e-5,f"R_asteroid / Rstar = {rarstar}")
+ax.text(100,0.5e-5,f"R_asteroid / Rstar = {rarstar:0.5}")
 ax.legend(loc="lower left")
 
 plt.show()
